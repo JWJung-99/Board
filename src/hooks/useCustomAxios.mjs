e199a0e298a343a4a6a28e35fcc2bf7d09868b17
JWJung-99 <39busy@naver.com>
2024-03-28 16:42:00 +0900

    modify: - refreshtoken

--- src/hooks/useCustomAxios.mjs
+++ src/hooks/useCustomAxios.mjs
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
+const REFRESH_URL = '/auth/refresh';
+
 function useCustomAxios() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -22,23 +24,47 @@ function useCustomAxios() {
   // 요청 인터셉터(성공, err)
   instance.interceptors.request.use((config) => {
     if (user) {
-      const accessToken = user.token.accessToken;
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      let token = user.token.accessToken;
+      if (config.url === REFRESH_URL) {
+        token = user.token.refreshToken;
+      }
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   });
 
   // 응답 인터셉터(성공, err)
-  instance.interceptors.response.use(res => res, err => {
-    if (err.response?.status === 401) {
-      const toLogin = confirm("로그인 후 이용 가능합니다. 로그인 하시겠습니까?");
-      toLogin && navigate('/users/login', {state: {from: location.pathname}});
+  instance.interceptors.response.use(res => res, async err => {
+    const { config, response } = err;
+    if (response?.status === 401) {
+      // refresh 토큰도 만료된 경우
+      if (config.url === REFRESH_URL) {
+        const toLogin = confirm("로그인 후 이용 가능합니다. 로그인 하시겠습니까?");
+        toLogin && navigate('/users/login', {state: {from: location.pathname}});
+      } else {
+        // access 토큰 재발급 요청
+        const accessToken = await getAccessToken(instance);
+        if (accessToken) {
+          config.headers.Authorization = `Bearer ${accessToken}`;
+          return axios(config);
+        }
+      }
+      
     } else {
       return Promise.reject(err);
     }
     
   });
+
+  async function getAccessToken(instance) {
+    try {
+      const {data: {accessToken}} = await instance.get(REFRESH_URL);
+      return accessToken;
+    } catch(err) {
+      console.error(err);
+    }
+  }
 
   return instance;
 }
